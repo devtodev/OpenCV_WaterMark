53ab59cc12e0c6c82cebe392a839ca0928b9a3ed
Karl <cmiguens@gmail.com>
2017-08-24 11:06:23 -0300

    Ver 0.1

---
+++ Debug/src/OpenCV_test1.d
@@ -0,0 +1,6 @@
+src/OpenCV_test1.d: ../src/OpenCV_test1.cpp \
+ /usr/local/include/opencv/cv.h /usr/local/include/opencv/highgui.h
+
+/usr/local/include/opencv/cv.h:
+
+/usr/local/include/opencv/highgui.h:
